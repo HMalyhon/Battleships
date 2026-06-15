de34aca111f6c0efef50007f10f07f4b7d0b1f96
HMalyhon <HMalyhon@gmail.com>
2026-06-15 19:38:44 +0200

    Make GameManager thread-safe (ConcurrentDictionary + per-game lock)

--- GameManager.cs
+++ GameManager.cs
@@ -1,46 +1,48 @@
 namespace Battleships;
 
+using System.Collections.Concurrent;
 using Models;
 
 public class GameManager
 {
-    private readonly Dictionary<string, BattleshipGame> games = new();
+    private readonly ConcurrentDictionary<string, BattleshipGame> _games = new();
+    private readonly ConcurrentDictionary<string, object> _locks = new();
 
     public string CreateNewGame()
     {
         var gameId = Guid.NewGuid().ToString();
         var game = new BattleshipGame();
         game.InitializeGame();
-        games[gameId] = game;
+        _games[gameId] = game;
+        _locks[gameId] = new object();
         return gameId;
     }
 
-    public bool GameExists(string gameId)
-    {
-        return games.ContainsKey(gameId);
-    }
+    public bool GameExists(string gameId) => _games.ContainsKey(gameId);
 
     public ShotResult ProcessShot(string gameId, string coordinate)
     {
-        var game = games[gameId];
-        return game.ProcessPlayerShot(coordinate);
+        lock (GetLock(gameId))
+            return _games[gameId].ProcessPlayerShot(coordinate);
     }
 
     public bool PlaceShip(string gameId, ShipPlacement placement)
     {
-        var game = games[gameId];
-        return game.PlacePlayerShip(placement);
+        lock (GetLock(gameId))
+            return _games[gameId].PlacePlayerShip(placement);
     }
 
     public GameBoard GetGameBoard(string gameId)
     {
-        var game = games[gameId];
-        return game.GetGameBoard();
+        lock (GetLock(gameId))
+            return _games[gameId].GetGameBoard();
     }
 
     public bool IsSetupComplete(string gameId)
     {
-        var game = games[gameId];
-        return game.IsSetupComplete();
+        lock (GetLock(gameId))
+            return _games[gameId].IsSetupComplete();
     }
-} 
+
+    private object GetLock(string gameId) => _locks[gameId];
+}
